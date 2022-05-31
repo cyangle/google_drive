4bcda7cbfa4de91c5f9b14df204cbc7125a93c0c
Chao Yang <cyang05@outlook.com>
2022-05-31 23:07:55 +0000

    Regenerate for form hash with IO value

--- src/google_drive/api_client.cr
+++ src/google_drive/api_client.cr
@@ -130,7 +130,7 @@ module GoogleDrive
     #
     # @return Crest::Request
     #   the data deserialized from response body (could be nil), response status code and response headers.
-    def build_api_request(http_method : Symbol, path : String, operation : String, post_body : IO | String?, auth_names = [] of String, header_params = {} of String => String, query_params = {} of String => (String | Array(String)), form_params : Hash(String, (String | Array(String) | ::File)) | Nil = {} of String => (String | Array(String) | ::File)) : Crest::Request
+    def build_api_request(http_method : Symbol, path : String, operation : String, post_body : IO | String?, auth_names = [] of String, header_params = {} of String => String, query_params = {} of String => (String | Array(String)), form_params : Hash(String, (String | Array(String) | IO)) | Nil = {} of String => (String | Array(String) | IO)) : Crest::Request
       # ssl_options = {
       #   "ca_file" => @config.ssl_ca_file,
       #   "verify" => @config.ssl_verify,
